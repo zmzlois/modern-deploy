f3af0cecced6fdd23436f2ed95ac8c0fefa9e9db
zmzlois <lois.z@comcord.vision>
2024-06-11 11:18:39 +0100

    comment out error code

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,17 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
-
+// import { withZephyr as ZephyrPlugin } from 'zephyr-webpack-plugin';
+// import type { PluginOptions } from '@modern-js/runtime/dist/types/core';
+import rspack from '@rspack/core';
 // https://modernjs.dev/en/configure/app/usage
+
+// function withZephyr(): PluginOptions<any, (...args: any[]) => void> {
+//   try {
+//     return ZephyrPlugin() as PluginOptions<any, (...args: any[]) => void>;
+//   } catch (err) {
+//     console.error('zephyr error', err);
+//     return ZephyrPlugin() as PluginOptions<any, (...args: any[]) => void>;
+//   }
+// }
 export default defineConfig({
   runtime: {
     router: true,
@@ -8,6 +19,9 @@ export default defineConfig({
   plugins: [
     appTools({
       bundler: 'experimental-rspack',
+    }),
+    new rspack.DefinePlugin({
+      //  zephyr: ZephyrPlugin(),
     }),
   ],
 });
